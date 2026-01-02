754d38339850c3d67711f7dbab85f171fa867d84
agathashaeed <165714940+agathashaeed@users.noreply.github.com>
2026-01-01 23:07:58 -0300

    Criar supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,17 @@
+// src/lib/supabase.ts
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Faltam VITE_SUPABASE_URL e VITE_SUPABASE_ANON_KEY no .env");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
